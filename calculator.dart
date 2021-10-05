e280cd49d420c65c978ef357000094a3a789bba6
Ahmed Hany <ahmedhanya1000@gmail.com>
2021-10-05 14:06:09 +0200

    changed the print method tocalculatorHandler function

--- calculator.dart
+++ calculator.dart
@@ -1,4 +1,4 @@
-import 'dart:io';
+
 
 class Calculator {
                                                                      //core functions
@@ -15,83 +15,6 @@ class Calculator {
       firstOperand ~/ secondOperand;
 
 
-                                                                        //Controller function
-  void printResultTerminal() {
-    print(
-        "\n              ---Linear Calculator---              \n              *************************              ");
-
-    String equation, operator = '';
-    int? firstOperand, secondOperand;
-
-    do {
-      bool continueSwitch = false;   //continue the loop outside the for each method becouse it doesn't support the break or continue
-      int i = 0;                     //counter for each charcter in the string used in the substring function to split the numbers and the operator
-      print(
-          "\nPlease Enter your Equation in the Shape a (+,-,*,/) b\n*****************************************************\n");
-      equation = stdin.readLineSync().toString();   //assign the user's equation to this variable
-
-      equation.runes.forEach((element) {            //foreach loop to seach for the operator then splits the first and the second operand
-        i++;
-
-        String value = String.fromCharCode(element);   // converts the value of the element from ascii code to string
-        if (value == '+' || value == '-' || value == '*' || value == '/') {
-          try {                                           // handle the wrong entry for first operand
-            firstOperand = int.parse(equation.substring(0, i - 1));     
-          } catch (e) {
-            print(
-                "ERROR: Please Enter an Interger Number in the First Operand\n");
-            continueSwitch = true;
-          }
-
-          try {                                            // handle the wrong entry for first operand
-            secondOperand = int.parse(equation.substring(i));        
-          } catch (e) {
-            print(
-                "ERROR: Please Enter an Interger Number in the Second Operand\n");
-            continueSwitch = true;
-          }
-
-          operator = equation.substring(i - 1, i);
-
-          i = 0;                                           // reset the counter to avoid the else if handler below
-        } else if (i == equation.length && equation != '0') {                   //handles the non operational equation
-          print("ERROR: Please Enter One of these Operators ( + , - , * , / )");
-          continueSwitch = true;
-        }
-        ;
-      });
-
-      if (continueSwitch || equation == '0') continue;                          //skip the swich if the foreach method cathes any errors
-
-      switch (operator) {                                                       //calculate and print the operation
-        case '+':
-          print("\nThe Result is ==> ${addition(
-              firstOperand: int.parse(firstOperand.toString()),
-              secondOperand: int.parse(secondOperand.toString()))}");
-          
-          break;
-        case '-':
-          print("\nThe Result is ==> ${subtraction(
-              firstOperand: int.parse(firstOperand.toString()),
-              secondOperand: int.parse(secondOperand.toString()))}");
-          
-          break;
-        case '*':
-          print("\nThe Result is ==> ${multiplication(
-              firstOperand: int.parse(firstOperand.toString()),
-              secondOperand: int.parse(secondOperand.toString()))}");
-          
-          break;
-        case '/':
-          print("\nThe Result is ==> ${division(
-              firstOperand: int.parse(firstOperand.toString()),
-              secondOperand: int.parse(secondOperand.toString()))}");
-         
-          break;
-
-        default:
-          print("must be handeled before!");
-      }
-    } while (equation != "0");                                                  //breakes when the user enter
-  }
+                                                                        
+  
 }
